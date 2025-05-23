91fa49c260758d29f2e5e5708e64e4507c09c4ba
unknown <nicolas.pachon-u@labinfo.is.escuelaing.edu.co>
2025-05-23 15:19:54 -0500

    added scafolding of project

---
+++ src/main/java/cvds/EciSaludVital/EciSaludVitalApplication.java
@@ -0,0 +1,13 @@
+package cvds.EciSaludVital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EciSaludVitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EciSaludVitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cvds/EciSaludVital/EciSaludVitalApplicationTests.java
@@ -0,0 +1,13 @@
+package cvds.EciSaludVital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EciSaludVitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
